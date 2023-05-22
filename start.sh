8527b20ba161ddcc6819fe38f9291863b04bafd9
Duc Hung <hunghd.uit@gmail.com>
2023-05-23 01:29:44 +0700

    update running command

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ docker network rm session-network
 docker network create session-network
 
 docker-compose -f ./session-api/docker-compose.yaml up --build -d
+docker-compose -f ./session-web/docker-compose.yaml up --build -d
--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker-compose -f ./session-api/docker-compose.yaml down --rmi all -v        
+docker-compose -f ./session-web/docker-compose.yaml down --rmi all -v        
 
 docker network rm session-network
